6ed6e64ca54ea2b9393a44f203ac3b4bd2657d46
Nawal <lulay2020@gmail.com>
2020-05-05 12:59:30 +0500

    Add review routes

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
-app.use('/reviews', reviewsRouter);
+app.use('/posts/:id/reviews', reviewsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
