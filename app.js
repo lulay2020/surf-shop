ac6d0d1e71971f090c5e6351a09c62b1f177345b
Nawal <lulay2020@gmail.com>
2020-05-05 12:31:36 +0500

    add post routes

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const postsRouter = require('./routes/posts');
+const reviewsRouter = require('./routes/reviews');
 
 const app = express();
 
@@ -22,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
+app.use('/reviews', reviewsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
