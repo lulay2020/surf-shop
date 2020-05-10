5349e8beea6288d50e9e4c06340bef68e33f007d
Nawal <lulay2020@gmail.com>
2020-05-10 14:43:44 +0500

    Add user authentication

--- app.js
+++ app.js
@@ -4,12 +4,31 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const User = require('./models/user');
+const session = require('express-session');
+const mongoose = require('mongoose');
 
+// require routes
 const indexRouter = require('./routes/index');
 const postsRouter = require('./routes/posts');
 const reviewsRouter = require('./routes/reviews');
 
 const app = express();
+
+// connect to database
+mongoose.connect('mongodb://localhost:27017/surf-shop', {
+		useNewUrlParser: true, 
+		useUnifiedTopology: true
+	});
+mongoose.set('useCreateIndex', true);
+const db = mongoose.connection;
+
+// check if database connected
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', ()=> {
+  console.log('we\'re connected!');
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +40,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// configuring passport and sessions
+
+app.use(session({
+	secret: 'the secret',
+	resave: false,
+	saveUninitialized: true
+}));
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+// mount routes
 app.use('/', indexRouter);
 app.use('/posts', postsRouter);
 app.use('/posts/:id/reviews', reviewsRouter);
